31a83ec4d94efea1e8ca346bcadea6cef95c413c
mhammadshami <mhammadshami3333@gmail.com>
2025-07-15 09:44:50 +0300

    feat(core): add global validation pipe and exception filter and unified error handling

--- edusphere-backend/src/main.ts
+++ edusphere-backend/src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { AllExceptionsFilter } from './test/all-exceptions-filter';
+import { ValidationPipe } from '@nestjs/common';
+import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionsFilter());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
